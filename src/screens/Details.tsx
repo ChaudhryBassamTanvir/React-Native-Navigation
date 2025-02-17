5f1f9d280a8c20a25428f5ca940658c558c065ae
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-16 23:53:41 -0800

    Updated Home and Details

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -11,4 +11,16 @@ const Details = () => {
 
 export default Details
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+
+
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  smallText:{
+    color:"#000000"
+  }
+})
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -14,4 +14,16 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+
+
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  smallText:{
+    color:"#000000"
+  }
+})
